79964ceb17676449e624c8efae2becb566bd9b7b
youngi08 <youngi.p08@gmail.com>
2017-10-08 21:31:23 +0900

    1152.cpp

---
+++ STR/1152.cpp
@@ -0,0 +1,30 @@
+/*
+  단어의 개수
+  https://www.acmicpc.net/problem/1152
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  string str;
+  getline(cin, str);
+
+  int cnt = 0;
+  for(int i=0; i<str.size(); i++){
+    if(str[i] == ' ') cnt++;
+  }
+
+  if(str.at(0) == ' '){
+    cnt--;
+  }
+
+  if(str.at(str.length()-1) == ' '){
+    cnt--;
+  }
+
+  cout << cnt+1 << endl;
+
+  return 0;
+}
